8f04c4de9faaed046039558b9e9d791cf821c013
Calvin107 <50202728+Calvin107@users.noreply.github.com>
2023-02-07 04:14:07 +0000

    Add files via upload

---
+++ round_corners_gui.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageDraw
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+
+class ImageCornerRounder:
+    def __init__(self, radius = 50):
+        self.radius = radius
+        self.image = None
+        self.path = None
+
+    def input_image(self):
+        self.path = filedialog.askopenfilename(title = "Input Image", filetypes=[("Image Files", "*.jpg *.jpeg *.png")])
+        self.image = Image.open(self.path)
+        message = "Image imported successfully"
+        messagebox.showinfo(title = "Selected File", message = message)
+
+    def run(self):
+        mask = Image.new("L", self.image.size)
+        draw = ImageDraw.Draw(mask)
+        draw.rounded_rectangle(((0, 0), self.image.size), self.radius, fill = 255)
+        self.image.putalpha(mask)
+        self.directory_path = os.path.dirname(self.path)
+        self.rounded_corner_image_name = "rounded_" + os.path.basename(self.path)
+        save_path = os.path.join(self.directory_path, self.rounded_corner_image_name)
+        self.image.save(save_path)
+        message = "Rounded corner image saved successfully"
+        messagebox.showinfo(title = "Selected File", message = message)
+        root.destroy()
+
+
+rounder = ImageCornerRounder(radius = 50)
+
+root = tk.Tk()
+root.title("Image Corner Rounder")
+root.resizable(False, False)
+root.geometry("300x150")
+
+input_button = tk.Button(root, text = "Input Image", command = rounder.input_image)
+run_button = tk.Button(root, text = "Run", command = rounder.run)
+
+input_button.pack(expand = True)
+run_button.pack(expand = True)
+
+root.mainloop()
